ac5e742e69dba1c73e2d7cc8af05e8bdc73338a7
Ondrej Sika <ondrej@ondrejsika.com>
2022-08-19 18:38:10 +0200

    feat(lib/api): Add Get, Set methods for /api/v1/

---
+++ lib/api/api.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func Set(origin, key, value string) error {
+	_, err := http.Post(
+		origin+"/api/v1/set/"+key,
+		"text/plain",
+		bytes.NewBuffer([]byte(value)),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func SetOrDie(origin, key, value string) {
+	err := Set(origin, key, value)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Get(origin, key string) (string, error) {
+	resp, err := http.Get(origin + "/api/v1/get/" + key)
+	if err != nil {
+		return "", err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	bodyStr := string(body)
+	return strings.TrimSuffix(bodyStr, "\n"), nil
+}
+
+func GetOrDie(origin, key string) string {
+	value, err := Get(origin, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return value
+}
