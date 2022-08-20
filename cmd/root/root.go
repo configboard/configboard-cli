0050d128bacdcada58fc00adeacd17108cc3f8eb
Ondrej Sika <ondrej@ondrejsika.com>
2022-08-20 14:54:48 +0200

    feat(cmd/root): Use Viper to get default value for --server param

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/configboard/configboard-cli/version"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var FlagServer string
@@ -14,17 +15,34 @@ var Cmd = &cobra.Command{
 	Short: "configboard-cli, " + version.Version,
 }
 
+func initViper() {
+	viper.SetEnvPrefix("CONFIGBOARD")
+
+	viper.SetConfigName(".configboard-cli")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	viper.ReadInConfig()
+}
+
 func init() {
+	initViper()
+
 	defaultServer := os.Getenv("CONFIGBOARD_SERVER")
 	if defaultServer == "" {
 		defaultServer = "http://127.0.0.1:8000"
 	}
 
+	viper.BindEnv("SERVER")
+	server := viper.GetString("SERVER")
 	Cmd.PersistentFlags().StringVarP(
 		&FlagServer,
 		"server",
 		"s",
-		defaultServer,
+		server,
 		"Configboard server",
 	)
+	if server == "" {
+		Cmd.MarkPersistentFlagRequired("server")
+	}
 }
