06fe4dece2820889ec37fd942810d52206019055
Ondrej Sika <ondrej@ondrejsika.com>
2022-08-20 16:08:51 +0200

    feat(cmd/get): Add --default value if it's not founf on server

--- cmd/get/get.go
+++ cmd/get/get.go
@@ -2,11 +2,14 @@ package get
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/configboard/configboard-cli/cmd/root"
 	"github.com/configboard/configboard-cli/lib/api"
 	"github.com/spf13/cobra"
 )
+
+var FlagDefault string
 
 var Cmd = &cobra.Command{
 	Use:   "get",
@@ -14,10 +17,26 @@ var Cmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Run: func(c *cobra.Command, args []string) {
 		path := args[0]
-		fmt.Printf("%s\n", api.GetOrDie(root.FlagServer, path))
+		value, err := api.Get(root.FlagServer, path)
+		if err != nil {
+			if FlagDefault != "" {
+				fmt.Printf("%s\n", FlagDefault)
+				return
+			}
+			log.Fatal(err)
+			return
+		}
+		fmt.Printf("%s\n", value)
 	},
 }
 
 func init() {
 	root.Cmd.AddCommand(Cmd)
+	Cmd.Flags().StringVarP(
+		&FlagDefault,
+		"default",
+		"d",
+		"",
+		"Default value if key is not found in ConfigBoard",
+	)
 }
